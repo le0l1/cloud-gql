200782166f02e7d6d41086c928b773a895afef70
leo <leo@192.168.0.107>
2019-05-12 23:16:27 +0800

    feat(User): Root Account &&  forbid register root

--- src/graphql/user/user.js
+++ src/graphql/user/user.js
@@ -22,6 +22,9 @@ export const createUserModel = db => ({
     address,
     role
   }) {
+    if (role === 3) {
+      throw new Error("Root Can`t Be Registerd");
+    }
     const client = await db.connect();
     try {
       const { hashed, salt } = hashPassword(password);
--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import dotenv from "dotenv";
 import path from "path";
 import { startServe } from "./app";
-import { getOperationDefinition, argumentsObjectFromField } from "apollo-utilities";
+import {
+  getOperationDefinition,
+  argumentsObjectFromField
+} from "apollo-utilities";
 
 // load enviroment
-const getEnvPath = () =>
-  path.resolve(process.cwd(), `.env.${process.env.MODE}`);
+const getEnvPath = () => path.resolve(process.cwd(), `.env`);
 
 dotenv.config({ path: getEnvPath() });
 
