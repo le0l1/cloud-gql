de91d4a84b0993fc4257fe7206f6729fd9fae983
leo.li <leezzxuan@gmail.com>
2019-06-29 18:25:02 +0800

    fix(image): decode error

--- src/graphql/image/image.js
+++ src/graphql/image/image.js
@@ -1,11 +1,11 @@
-import { decodeIDAndType, decodeNumberId } from '../../helper/util';
+import { decodeIDAndType } from '../../helper/util';
 import { Shop } from '../shop/shop.entity';
 import { Image } from './image.entity';
 
 export default class ImageResolver {
   static async searchImages({ imageTypeId }) {
     const [_, typeId] = decodeIDAndType(imageTypeId);
-    const shop = await Shop.findOneOrFail(decodeNumberId(typeId));
+    const shop = await Shop.findOneOrFail(typeId);
     return Image.find({
       shop,
     });
