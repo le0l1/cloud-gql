2e52310a630419581c784690e439699ff8b0fd7f
leo.li <leezzxuan@gmail.com>
2019-07-15 23:11:37 +0800

    refactor(order): wxpay

--- src/graphql/order/order.route.js
+++ src/graphql/order/order.route.js
@@ -2,14 +2,12 @@ import Router from 'koa-router';
 import { js2xml } from 'xml-js';
 import { getManager } from 'typeorm';
 import { mapObjectArr, env } from '../../helper/util';
-import { User } from '../user/user.entity';
 import { Order } from './order.entity';
 import { OrderDetail } from './orderDetail.entity';
-import { Shop } from '../shop/shop.entity';
-import logger from '../../helper/logger';
 import { WXPay } from '../payment/wxpay';
 import { OrderStatus, PaymentStatus } from '../../helper/status';
 import { Payment } from '../payment/payment.entity';
+import { OrderLog } from './orderLog.entity';
 
 const router = new Router();
 
@@ -27,56 +25,76 @@ const failResult = js2xml(
   },
   { compact: true, ignoreComment: true, spaces: 4 },
 );
+/**
+ *  根据订单号获取订单
+ * @param  {number} orderNumber 订单号
+ */
+const getOrder = orderNumber => Order.createQueryBuilder('order')
+  .leftJoinAndMapMany(
+    'order.orderDetail',
+    OrderDetail,
+    'orderDetail',
+    'orderDetail.orderId = order.id',
+  )
+  .where('order.orderNumber = :orderNumber', {
+    orderNumber,
+  })
+  .getOne();
+/**
+ *  记录异常订单
+ * @param {*} order 订单实例
+ * @param {*} description 描述
+ * @param {*} trx 事务实例
+ */
+const recordUnusalOrder = (order, description, trx) => trx.save(OrderLog, {
+  orderId: order.id,
+  oldStatus: order.status,
+  newStatus: OrderStatus.UNUSUAL,
+  description,
+});
 
 router.post(
   env('WXPAY_ORDER_NOTIFY_URL'),
+  // 处理签名失败
   async (ctx, next) => {
-    ctx.type = 'application/xml';
     const xml = mapObjectArr(ctx.request.body.xml);
     if (checkReqStatus(xml)) {
       ctx.body = failResult;
       return;
     }
-    logger.info('校验微信签名');
-    // const { sign, ...rest } = xml;
-    // const wxpay = new WXPay(rest);
-    // if (wxpay.sign !== sign) {
-    //   logger.error('微信签名校验失败!');
-    //   ctx.body = failResult;
-    //   return;
-    // }
+    const { sign, ...rest } = xml;
+    const wxpay = new WXPay(rest);
+    if (wxpay.sign !== sign) {
+      ctx.failReson = '微信签名校验失败';
+    }
+    ctx.xml = xml;
+    next();
+  },
+  async (ctx, next) => {
     await getManager().transaction(async (trx) => {
-      const order = await Order.createQueryBuilder('order')
-        .leftJoinAndMapMany(
-          'order.orderDetail',
-          OrderDetail,
-          'orderDetail',
-          'orderDetail.orderId = order.id',
-        )
-        .where('order.orderNumber = :orderNumber', {
-          orderNumber: xml.out_trade_no,
-        })
-        .getOne();
-      logger.info('检查订单状态:', order.status);
-      if (order.status !== OrderStatus.PENDING) {
-        return next();
+      const { xml } = ctx;
+      const order = await getOrder(xml.out_trade_no);
+      if (xml.failReson) {
+        await recordUnusalOrder(order, xml.failReson, trx);
+        await trx.merge(Order, order, { status: OrderStatus.UNUSUAL }).save();
+        ctx.body = failResult;
+        return;
       }
-      logger.info('当前订单号:', order.orderNumber);
-      logger.info('订单总金额:', order.totalCount);
-      logger.info('优惠金额:', order.discount);
-      logger.info('微信支付金额(分)', xml.total_fee);
-      logger.info(`修改支付记录 ${order.paymentId} 状态为已支付`);
-      await trx.update(Payment, order.paymentId, { paymentStatus: PaymentStatus.PAID });
-      logger.info(`修改订单 ${order.id} 状态为待发货`);
-      // TODO: 如果金额不匹配 则按照实际支付金额处理
+      if (order.status !== OrderStatus.PENDING) {
+        next();
+        return;
+      }
       if (Number(order.totalCount) - Number(order.discount) !== Number(xml.total_fee) / 1000) {
         const totalCount = Number(xml.total_fee) / 1000 + Number(order.discount);
-        logger.info('订单金额不匹配,修改为实际支付金额 + 优惠金额:', totalCount);
-        order.totalCount = totalCount;
+        const msg = `订单金额不匹配,修改为实际支付金额 + 优惠金额: ${totalCount}`;
+        await recordUnusalOrder(order, msg, trx);
+        await trx.merge(Order, order, { status: OrderStatus.UNUSUAL }).save();
+        next();
+        return;
       }
       order.status = OrderStatus.WAIT_SHIP;
+      await trx.update(Payment, order.paymentId, { paymentStatus: PaymentStatus.PAID });
       await trx.save(order);
-      logger.info('订单完成');
       next();
     });
   },
