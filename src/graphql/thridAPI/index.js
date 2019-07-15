03073c09dabaa094ddec2e2b8bfe8456fc71d69e
leo.li <leezzxuan@gmail.com>
2019-07-15 12:59:37 +0800

    chore: smsCode log

--- src/graphql/thridAPI/index.js
+++ src/graphql/thridAPI/index.js
@@ -4,6 +4,7 @@ import { sendSMS } from '../../helper/thirdAPI/sms';
 import { generateUploadToken } from '../../helper/thirdAPI/oss';
 import { generateSMSCode } from '../../helper/util';
 import { SMSCode } from './smsCode.entity';
+import logger from '../../helper/logger';
 
 const resolvers = {
   Query: {
@@ -14,6 +15,7 @@ const resolvers = {
       const smsCode = generateSMSCode();
       try {
         await sendSMS({ phone, smsCode });
+        logger.info(`手机号码: ${phone}, 验证码: ${smsCode}`);
         await SMSCode.save({
           phone,
           smsCode,
