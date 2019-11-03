f69df9a402ce041618b459d4279aa68edfd8e105
leo.li <leezzxuan@gmail.com>
2019-11-03 12:48:16 +0800

    feat(statistics): default value

--- src/graphql/statistics/index.js
+++ src/graphql/statistics/index.js
@@ -7,6 +7,20 @@ const resolvers = {
       return handleStatisticsSearch(query);
     },
   },
+  Statistics: {
+    userCount(v) {
+      return v ? v.userCount : 0;
+    },
+    phoneCount(v) {
+      return v ? v.phoneCount : 0;
+    },
+    orderCount(v) {
+      return v ? v.orderCount : 0;
+    },
+    moneyCount(v) {
+      return v ? v.moneyCount : 0;
+    },
+  },
 };
 
 export default {
