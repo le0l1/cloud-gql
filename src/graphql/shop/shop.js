1ec5a1846edab769436a9fcfc5a3a4e878acf6b7
Leo Li <leo.li@chongplus.cn>
2019-12-04 10:46:48 +0800

    fix(shop): filter deleted shop

--- src/graphql/shop/shop.js
+++ src/graphql/shop/shop.js
@@ -173,15 +173,15 @@ export default class ShopResolver {
   }) {
     return pipe(
       getQB('shop'),
+      where('shop.deletedAt is null'),
       leftJoinAndMapOne('category.shopCategory', ShopCategory, 'shopCategory', 'shop.id = shopCategory.shopId'),
       leftJoinAndMapMany('shop.categories', Category, 'category', 'category.id = shopCategory.categoryId'),
       leftJoinAndMapMany('shop.phoneRecords', PhoneRecord, 'phoneRecord', 'phoneRecord.shopId = shop.id'),
       leftJoinAndSelect('shop.phones', 'phone'),
-      where('shop.deletedAt is null'),
       where('shop.city = :city', {
         city,
       }),
-      where('shop.name like :tsQuery or category.name like :tsQuery', {
+      where('(shop.name like :tsQuery or category.name like :tsQuery)', {
         tsQuery: tsQuery ? `%${tsQuery}%` : null,
       }),
       where('shop.status = :status', { status: filter.status }),
